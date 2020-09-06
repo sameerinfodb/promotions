70f2d36fbada1016fbded254fae649b03411de7d
Sameer Ahmed S <sameerinfodb@gmail.com>
2020-09-06 19:25:12 +0530

    added promotion application

---
+++ apps/promotion/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sample1-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'promotion';
+}
